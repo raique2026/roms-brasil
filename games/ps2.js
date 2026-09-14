2d5bf744046899fca8e56e8676b1718327c73391
raique2026 <rjs12raique@gmail.com>
2026-09-14 00:43:54 -0300

    Update ps2.js

--- games/ps2.js
+++ games/ps2.js
@@ -1,1 +1,177 @@
+/* ===== JOGOS DE PLAYSTATION 2 ===== */
+const ps2Games = [
 
+  {
+      slug: "need-for-speed-most-wanted-black-edition",
+      title: "Need for Speed: Most Wanted - Black Edition",
+      platform: "PS2",
+      year: "2005",
+      language: "Portugues",
+      region: "NTSC-U/C",
+      size: "2.85 GB",
+      developer: "EA Canada",
+      publisher: "Electronic Arts",
+      genre: "Corrida",
+      format: "ISO",
+      translation: "Diogo Amaral - RafaSZ e ZANIT",
+  
+      cover: "images/need-for-speed-most-wanted-black-edition.jpg",
+  
+      banner: "banners/need-for-speed-most-wanted-black-edition.png",
+  
+      description: "Need for Speed: Most Wanted - Black Edition é uma edição especial de Most Wanted, com conteúdos extras e foco em corridas de rua e perseguições policiais.",
+  
+      screenshots: [
+        "screenshots/need-for-speed-most-wanted-black-edition/screenshot-01.png",
+        "screenshots/need-for-speed-most-wanted-black-edition/screenshot-02.png",
+        "screenshots/need-for-speed-most-wanted-black-edition/screenshot-03.png"
+      ],
+  
+      download: "https://gofile.io/d/0FxG10Az"
+    },
+
+  {
+    slug: "midnight-club-3-dub-edition-remix",
+    title: "Midnight Club 3: DUB Edition Remix",
+    platform: "PS2",
+    year: "2006",
+    language: "Portugues",
+    region: "NTSC-U/C",
+    size: "7.95 GB",
+    developer: "Rockstar San Diego",
+    publisher: "Rockstar Games",
+    genre: "Corrida",
+    format: "ISO",
+      translation: "Não informado",
+  
+    cover: "images/midnight-club-3-dub-edition-remix.jpg",
+  
+    banner: "banners/midnight-club-3-dub-edition-remix.png",
+  
+    description: "Midnight Club 3: DUB Edition Remix é uma versão expandida do jogo de corrida da Rockstar, com novos veículos, músicas, corridas e a cidade de Tóquio adicionada ao conteúdo original.",
+  
+    screenshots: [
+      "screenshots/midnight-club-3-dub-edition-remix/screenshot-01.png",
+      "screenshots/midnight-club-3-dub-edition-remix/screenshot-02.png",
+      "screenshots/midnight-club-3-dub-edition-remix/screenshot-03.png"
+    ],
+  
+    download: "#"
+  },
+
+  {
+    slug: "god-of-war-2",
+    title: "God of War II",
+    platform: "PS2",
+    year: "2007",
+    language: "Português",
+    region: "NTSC-U/C",
+    size: "Edite o tamanho",
+    developer: "Santa Monica Studio",
+    publisher: "Sony Computer Entertainment",
+    genre: "Ação / Aventura",
+    format: "ISO",
+      translation: "Não informado",
+  
+    cover: "images/god-of-war-2.jpg",
+  
+    description: "God of War II continua a jornada de Kratos em uma aventura épica pela mitologia grega, com novos inimigos, armas, poderes e batalhas grandiosas.",
+  
+    screenshots: [
+      "screenshots/god-of-war-2/screenshot-01.png",
+      "screenshots/god-of-war-2/screenshot-02.png",
+      "screenshots/god-of-war-2/screenshot-03.png"
+    ],
+  
+    banner: "banners/god-of-war-2.jpg",
+  
+    download: "#"
+  },
+
+  {
+    slug: "need-for-speed-underground",
+    title: "Need for Speed: Underground",
+    platform: "PS2",
+    year: "2003",
+    language: "Português",
+    region: "NTSC-U/C",
+    size: "Edite o tamanho",
+    developer: "EA Black Box",
+    publisher: "Electronic Arts",
+    genre: "Corrida",
+    format: "ISO",
+      translation: "Não informado",
+  
+    cover: "images/need-for-speed-underground.jpg",
+  
+    description: "Need for Speed: Underground leva o jogador ao mundo das corridas de rua, com personalização de veículos, diversos eventos e uma campanha ambientada na cultura tuning.",
+  
+    banner: "banners/need-for-speed-underground.jpg",
+  
+    screenshots: [
+      "screenshots/need-for-speed-underground/screenshot-01.png",
+      "screenshots/need-for-speed-underground/screenshot-02.png",
+      "screenshots/need-for-speed-underground/screenshot-03.png"
+    ],
+  
+    download: "#"
+  },
+
+  {
+    slug: "ben-10-protector-of-earth",
+    title: "Ben 10: Protector of Earth",
+    platform: "PS2",
+    year: "2007",
+    language: "Português",
+    region: "NTSC-U/C",
+    size: "Edite o tamanho",
+    developer: "High Voltage Software",
+    publisher: "D3 Publisher",
+    genre: "Ação / Aventura",
+    format: "ISO",
+      translation: "Não informado",
+  
+    cover: "images/ben-10-protector-of-earth.jpg",
+  
+    description: "Ben 10: Protector of Earth é um jogo de ação e aventura baseado na série Ben 10. Ben utiliza o Omnitrix para se transformar em diferentes alienígenas e enfrentar inimigos durante sua jornada.",
+  
+    banner: "banners/ben-10-protector-of-earth.jpg",
+  
+    screenshots: [
+      "screenshots/ben-10-protector-of-earth/screenshot-01.png",
+      "screenshots/ben-10-protector-of-earth/screenshot-02.png",
+      "screenshots/ben-10-protector-of-earth/screenshot-03.png"
+    ],
+  
+    download: "#"
+  },
+
+  {
+    slug: "resident-evil-4",
+    title: "Resident Evil 4",
+    platform: "PS2",
+    year: "2005",
+    language: "Português",
+    region: "NTSC-U/C",
+    size: "4.37 GB",
+    developer: "Capcom",
+    publisher: "Capcom",
+    genre: "Survival Horror",
+    format: "ISO",
+      translation: "Não informado",
+  
+    cover: "images/resident-evil-4.jpg",
+  
+    description: "Resident Evil 4 acompanha Leon S. Kennedy em uma missão para resgatar a filha do presidente dos Estados Unidos em uma misteriosa região rural da Europa.",
+  
+    banner: "banners/resident-evil-4.jpg",
+  
+    screenshots: [
+      "screenshots/resident-evil-4/screenshot-01.png",
+      "screenshots/resident-evil-4/screenshot-02.png",
+      "screenshots/resident-evil-4/screenshot-03.png"
+    ],
+  
+    download: "https://gofile.io/d/TRkXcw"
+  }
+];
